3f957b7fdf9dd94b463b5a93910b8200a0bebd68
nouhirh@gmail.com <nouhirh@gmail.com>
2023-01-25 18:30:19 +0100

    last

---
+++ components/ProductItem.js
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function ProductItem({ product }) {
+    return (
+        <div className='cart'>
+            <Link href={`/product/${product.slug}`}>
+                <div>
+                    <img
+                        src={product.image}
+                        alt={product.name}
+                        className="rounded shadow"
+                    />
+                </div>
+            </Link>
+            <div className='flex flex-col items-center justify-center'>
+                <Link href={`/product/${product.slug}`}>
+                    <h2 className='text-lg'>{product.name}</h2>
+                </Link>
+                <p className='mb-2'>{product.brand}</p>
+                <p>${product.price}</p>
+                <button className='primary-button' type='button'>Add to cart</button>
+            </div>
+        </div>
+    )
+}
---
+++ utils/data.js
@@ -0,0 +1,102 @@
+const data = {
+    product: [
+        {
+            name: 'Nike Shirt',
+            slug: 'nike-shirt',
+            category: 'Shirts',
+            image: 'images/shirt1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 9,
+            countInStock: 66,
+            description: 'A popular shirt'
+        },
+        {
+            name: 'Adidas Shirt',
+            slug: 'adidas-shirt',
+            category: 'Shirts',
+            image: '/images/shirt2.jpg',
+            price: 80,
+            brand: 'adidas',
+            rating: 4.1,
+            numReviews: 5,
+            countInStock: 20,
+            description: 'A popular shirt'
+        },
+        {
+            name: 'Reef Shirt',
+            slug: 'reef-shirt',
+            category: 'Shirts',
+            image: '/images/shirt3.jpg',
+            price: 70,
+            brand: 'reef',
+            rating: 4.3,
+            numReviews: 20,
+            countInStock: 100,
+            description: 'A popular shirt'
+        },
+        {
+            name: 'Reef Shirt',
+            slug: 'reef-shirt',
+            category: 'Shirts',
+            image: '/images/shirt3.jpg',
+            price: 70,
+            brand: 'reef',
+            rating: 4.3,
+            numReviews: 20,
+            countInStock: 100,
+            description: 'A popular shirt'
+        },
+        {
+            name: 'Reef Shirt',
+            slug: 'reef-shirt',
+            category: 'Shirts',
+            image: '/images/shirt3.jpg',
+            price: 70,
+            brand: 'reef',
+            rating: 4.3,
+            numReviews: 20,
+            countInStock: 100,
+            description: 'A popular shirt'
+        },
+        {
+            name: 'Reef Shirt',
+            slug: 'reef-shirt',
+            category: 'Shirts',
+            image: '/images/shirt3.jpg',
+            price: 70,
+            brand: 'reef',
+            rating: 4.3,
+            numReviews: 20,
+            countInStock: 100,
+            description: 'A popular shirt'
+        },
+        {
+            name: 'Reef Shirt',
+            slug: 'reef-shirt',
+            category: 'Shirts',
+            image: '/images/shirt3.jpg',
+            price: 70,
+            brand: 'reef',
+            rating: 4.3,
+            numReviews: 20,
+            countInStock: 100,
+            description: 'A popular shirt'
+        },
+        {
+            name: 'Reef Shirt',
+            slug: 'reef-shirt',
+            category: 'Shirts',
+            image: '/images/shirt3.jpg',
+            price: 70,
+            brand: 'reef',
+            rating: 4.3,
+            numReviews: 20,
+            countInStock: 100,
+            description: 'A popular shirt'
+        },
+    ]
+}
+
+export default data;
